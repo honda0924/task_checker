4ba644990facb1a121bc3d0eba1ed531818db3f7
nekoyashiki <47511135+honda0924@users.noreply.github.com>
2021-03-28 16:00:55 +0900

    create header

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
+import './style.css';
+import { Header } from '../../components/header';
 
 export const Home = ()=> {
   return (
-    <div>
-      <h1>
-        HelloWorld
-      </h1>
-    </div>
+    <Header></Header>
   )
 }
