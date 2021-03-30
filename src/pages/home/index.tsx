8d36724b3d2319dab8f726e021a9046876f6bd4c
nekoyashiki <47511135+honda0924@users.noreply.github.com>
2021-03-30 23:55:58 +0900

    modal

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './style.css';
 import { Header } from '../../components/header';
 import { Select } from '../../components/select';
 import { ToDoList } from '../../components/toDoList';
-
+import { FormModal } from "../../components/modal";
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 
 
 export const Home = ()=> {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div className="main">
       <Header />
       <div className="genre">
         <Select />  
-        <AddCircleOutlineIcon className="add_circle_outline_icon" fontSize="default" />
+        <AddCircleOutlineIcon 
+          className="add_circle_outline_icon" 
+          fontSize="default" 
+          onClick={handleOpen}
+        />
       </div>
+        <FormModal handleClose={handleClose} isOpen={isOpen} />
       <div className="contents">
         <ToDoList />
       </div>
